5410a4a3af566035c2531eb1451acc3d92c20061
ancgarciamo <51928175+ancgarciamo@users.noreply.github.com>
2022-11-29 14:10:15 -0500

    Add files via upload

--- sistema_recomendacion.py
+++ sistema_recomendacion.py
@@ -23,7 +23,7 @@ lista_videojuegos = load_modelA()
 def main():
     df = pd.read_csv('base_original.csv', encoding="utf-8")
     df2 = pd.read_csv('base_actualizada.csv', encoding="utf-8")
-
+    st.text("por favor mire la lista de usuarios ya creados abajo , evite ingresar nombres que ya estan o repetir nombres , de lo contrario la aplicacion tendra comportamientos inesperados")
     st.title("Ingresar usuario")
     options = st.multiselect(
         'Seleccione los juegos que ha jugado',
@@ -33,7 +33,11 @@ def main():
     st.write('You selected:', options)
     indice = []
     variable = []
+
     Nombre = st.text_input("Ingrese su Nombre")
+
+
+
 
     for i in options:
         valoracion = st.slider("Ingrese la valoracion de " + str(i), 0, 10)
